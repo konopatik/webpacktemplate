d0f643e739782800109735d08f4dafbec8eacc11
konopatik <sonya.onoprienko@gmail.com>
2021-01-07 16:44:26 +0300

    Верстка карточки комнаты(страница Cards)

--- src/js/common.js
+++ src/js/common.js
@@ -1,2 +1,16 @@
-let add = (a,b) => a*b;
-console.log(add(2,7))
+const products = document.querySelectorAll('.product');
+
+
+if (products) {
+    products.forEach(el => {
+        const imageSwitchItems = el.querySelectorAll('.image-switch__item');
+        const imagePagination = el.querySelector('.image-pagination');
+
+        if (imageSwitchItems.length > 1) {
+            imageSwitchItems.forEach((el, index) => {
+                el.setAttribute('data-index', index);
+                imagePagination.innerHTML += `<li class="image-pagination__item ${index == 0 ?'image-pagination__item--active': ''} data-index="${index}"></li>`;
+            })
+        }
+    })
+}
