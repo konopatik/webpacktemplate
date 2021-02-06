9357884f274f11ff4cc4f7c91fd546d39bc7e1b4
konopatik <sonya.onoprienko@gmail.com>
2021-02-06 16:08:29 +0300

    рефакторинг scss(закончила). переделала структуру файлов(требует доработки)

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@
 // import "./js/calendar_day_of_week"
 import "./js/buttons"
 import "./assets/scss/main.scss"
+import "./js/test"
--- src/js/buttons.js
+++ src/js/buttons.js
@@ -8,3 +8,4 @@ const buttons = document.querySelectorAll('.buttons');
      })
 
  }
+
