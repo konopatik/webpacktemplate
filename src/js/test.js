f5d73804bdad9e98fdd1266debbb7b37c82a922b
konopatik <sonya.onoprienko@gmail.com>
2021-02-13 15:37:17 +0300

    настройка ionrange slider

--- src/js/test.js
+++ src/js/test.js
@@ -1,18 +1,35 @@
 import 'ion-rangeslider';
 import 'ion-rangeslider/css/ion.rangeSlider.min.css';
-$(document).ready(function($){
-    $(".js-range-slider").css('background', 'blue')
-});
-// $(".js-range-slider").ionRangeSlider();
 
-// {
-//     skin: "big",
-//         min: 0,
-//     max: 10000,
-//     from: 1000,
-//     to: 9000,
-//     type: 'double',
-//     prefix: "$",
-//     grid: true,
-//     grid_num: 10
-// }
+
+function logslider(position, reverse) {
+    // position will be between 0 and 100
+    var minp = 0;
+    var maxp = 100;
+
+    // The result should be between 100 an 10000000
+    var minv = Math.log(10);
+    var maxv = Math.log(10000);
+
+    // calculate adjustment factor
+    var scale = (maxv - minv) / (maxp - minp);
+
+    return reverse
+        ? (Math.log(position) - minv) / scale + minp
+        : Math.exp(minv + scale * (position - minp))
+        ;
+}
+
+
+$(".js-range-slider").ionRangeSlider({
+    step: 1000,
+    type: "double",
+    grid: true,
+    min: 0,
+    max: 15000,
+    from: 5000,
+    to: 5000,
+    prettify(position) {
+        return Math.round(logslider(position, false)).toLocaleString();
+    }
+})
